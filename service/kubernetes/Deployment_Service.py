cd8432a6009c05f2ed5752e51b98fc54c95f85ee
yavuz kaymak <yavuz.kaymak@otto.de>
2023-11-21 13:23:10 +0100

    (fix) indent

--- service/kubernetes/Deployment_Service.py
+++ service/kubernetes/Deployment_Service.py
@@ -1,11 +1,11 @@
 from model import Deployment
 
+
 def delete(deployment: Deployment):
     print(f"Deployment {deployment.name} has been deleted")
+
 
 def create(deployment: Deployment):
     print(f"Deployment {deployment.name} has been created with {deployment.spec.replica} replicas")
 
-    print(f"Here is the json representaton: {deployment.model_dump_json()}")
-
-
+    print(f"Here is the json representation: {deployment.model_dump_json()}")
